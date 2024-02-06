314e92ebe1b0cfc1d163369d2101bfbfc812048d
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2024-02-06 12:08:57 +0200

    ft: added images

---
+++ packages/Images/index.ts
@@ -0,0 +1,14 @@
+import output from './output.png'
+import agcv from './agcv1.png'
+import stars from './stars10ly.png'
+import saturn from './Francisco_de_Goya_Saturno_devouring.jpg'
+import quarks from './quarks1.jpg'
+
+const images =  {
+    output,
+    agcv,
+    stars,
+    saturn,
+    quarks
+}
+export default images
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,10 +1,11 @@
-import { H1, H2, H3, Paragraph, Square, Theme, YStack, styled, useMedia } from '@my/ui'
+import { H1, H2, H3, Paragraph, Square, Theme, YStack, styled, useMedia, Image } from '@my/ui'
 import { ArticleBox, ArticleBoxSM, Layout1, P1 } from '../../components'
+import images from '../../../Images'
 
 const ARTICLES = [
   {
     title: 'Star types: universe composition',
-    titleMobile: "Star types: universe composition",
+    titleMobile: 'Star types: universe composition',
     text: `Each one of those stars and in fact almost everything around you, yourself included is
     just made up of those two quarks. Quarks have a bit of a weird charge. 2/3 for the UP
     quark and -1/2 for the DOWN one. You can remember that "up" means positive and "down"
@@ -12,11 +13,12 @@ const ARTICLES = [
     textMobile: `Each one of those stars and in fact almost everything around you, yourself included is
     just made up of those two quarks. Quarks have a bit of a weird charge. 2/3 for the UP
     quark and -1/2 for the DOWN one. You can remember that "up" means positive and "down"
-    means negative, and the "up" means more charge than the "down". So if you take two UP...`
+    means negative, and the "up" means more charge than the "down". So if you take two UP...`,
+    image: images.quarks.src
   },
   {
     title: 'Star types: universe composition',
-    titleMobile: "Star types: universe composition",
+    titleMobile: 'Star types: universe composition',
     text: `Each one of those stars and in fact almost everything around you, yourself included is
     just made up of those two quarks. Quarks have a bit of a weird charge. 2/3 for the UP
     quark and -1/2 for the DOWN one. You can remember that "up" means positive and "down"
@@ -24,11 +26,12 @@ const ARTICLES = [
     textMobile: `Each one of those stars and in fact almost everything around you, yourself included is
     just made up of those two quarks. Quarks have a bit of a weird charge. 2/3 for the UP
     quark and -1/2 for the DOWN one. You can remember that "up" means positive and "down"
-    means negative, and the "up" means more charge than the "down". So if you take two UP...`
+    means negative, and the "up" means more charge than the "down". So if you take two UP...`,
+    image: images.quarks.src
   },
   {
     title: 'Star types: universe composition',
-    titleMobile: "Star types: universe composition",
+    titleMobile: 'Star types: universe composition',
     text: `Each one of those stars and in fact almost everything around you, yourself included is
     just made up of those two quarks. Quarks have a bit of a weird charge. 2/3 for the UP
     quark and -1/2 for the DOWN one. You can remember that "up" means positive and "down"
@@ -36,8 +39,9 @@ const ARTICLES = [
     textMobile: `Each one of those stars and in fact almost everything around you, yourself included is
     just made up of those two quarks. Quarks have a bit of a weird charge. 2/3 for the UP
     quark and -1/2 for the DOWN one. You can remember that "up" means positive and "down"
-    means negative, and the "up" means more charge than the "down". So if you take two UP...`
-  }
+    means negative, and the "up" means more charge than the "down". So if you take two UP...`,
+    image: images.quarks.src
+  },
 ]
 
 const Mobile = () => {
@@ -47,9 +51,7 @@ const Mobile = () => {
         <ArticleBoxSM key={index} my={200} mx={4}>
           <YStack>
             <H1>{article.titleMobile}</H1>
-            <P1>
-              {article.textMobile}
-            </P1>
+            <P1>{article.textMobile}</P1>
           </YStack>
         </ArticleBoxSM>
       ))}
@@ -57,25 +59,35 @@ const Mobile = () => {
   )
 }
 const Desktop = () => {
+  const handlePress = () => {
+    console.log('pressed')
+  }
   return (
     <Layout1>
       {ARTICLES.map((article, index) => {
-        const isLastItem = index === ARTICLES.length-1
+        const isLastItem = index === ARTICLES.length - 1
         const isFirstItem = index === 0
         return (
-        <ArticleBox 
-          key={index} 
-          mt={isFirstItem ? 200 : 20} 
-          mb={isLastItem ? 200 : 0 }
-        >
-          <YStack>
-            <H2 mb="$4">{article.title}</H2>
-            <P1 mb={50}>
-              {article.text}
-            </P1>
-          </YStack>
-        </ArticleBox>
-      )})}
+          <ArticleBox 
+            key={index} 
+            mt={isFirstItem ? 200 : 20} 
+            mb={isLastItem ? 200 : 0}
+            onPress={handlePress}
+          >
+            <YStack>
+              <Image
+                source={{
+                  uri: article.image,
+                  width: "100%",
+                  height: 300,
+                }}
+              />
+              <H2 mb="$4">{article.title}</H2>
+              <P1 mb={50}>{article.text}</P1>
+            </YStack>
+          </ArticleBox>
+        )
+      })}
     </Layout1>
   )
 }
