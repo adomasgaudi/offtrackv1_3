6ab98480ce652e58da5b29d8a9d5b3935c2ecd5f
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2024-02-16 14:07:07 +0200

    ft: CV sheet update, project organise

--- packages/app/features/home/homePack/index.tsx
+++ packages/app/features/home/homePack/index.tsx
@@ -15,6 +15,7 @@ import {
   YStack,
   Image,
   View,
+  Text,
 } from '@my/ui'
 import { Theme } from 'tamagui'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
@@ -48,7 +49,7 @@ function Skill({ title, skills }) {
   )
 }
 
-function SheetDemo() {
+function CVSheet() {
   const [open, setOpen] = useState(false)
   const [position, setPosition] = useState(0)
   const toast = useToastController()
@@ -68,12 +69,18 @@ function SheetDemo() {
         onPositionChange={setPosition}
         dismissOnSnapToBottom
       >
-        <Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
-        <Sheet.Frame ai="center" jc="center">
+        <Sheet.Frame
+          ai="center"
+          jc="center"
+          shadowRadius={2}
+          shadowOffset={2}
+          shadowColor="black"
+          shadowOpacity={0.3}
+          bg="#fdfdfd"
+        >
           <Sheet.Handle />
           <Button
-            size="$6"
-            circular
+            size="$2"
             icon={ChevronDown}
             onPress={() => {
               setOpen(false)
@@ -81,29 +88,28 @@ function SheetDemo() {
                 message: 'I sometimes forget to read emails',
               })
             }}
+            mb={10}
+            bg="#fdfdfd"
           />
-          <View w="100%" bg="red" h="100%" p={10}>
 
-          <View w="100%" bg="blue" h="100%">
-          <H1>jhgjhgj</H1>
-
-          <H1>jhgjhgj</H1>
-          </View>
-          </View>
-          {/* <ContactInfo /> */}
+          <CuriculumVitae />
         </Sheet.Frame>
       </Sheet>
     </>
   )
 }
 
-function ContactInfo() {
+function CuriculumVitae() {
+  const media = useMedia()
   return (
-    <YStack>
-      <H2>Curriculum Vitae</H2>
-      <Image source={{ width: 650, height: 800, uri: img.agcv.src }} my={100} />
-      <Paragraph ta="center">adomas.gaudi.23@gmail.com</Paragraph>
-      <Paragraph ta="center">+370 61609911</Paragraph>
+    <YStack w="100%" h={500} ai="center">
+      <Image
+        source={{ width: media.xs ? '100%' : 600, height: media.xs ? 500 : 800, uri: img.agcv.src }}
+        shadowRadius={2}
+        shadowOffset={2}
+        shadowColor="black"
+        shadowOpacity={0.2}
+      />
     </YStack>
   )
 }
@@ -133,16 +139,65 @@ const Boxy = ({ children, mobile }) => {
   )
 }
 
+const Project = ({ href, imgSrc, title, descriptions, media }) => (
+  <Anchor href={href} target="_blank">
+    <YStack position="relative" mb={150}>
+      <Image
+        source={{
+          uri: imgSrc,
+          width: '100%',
+          height: media.sm ? 200 : 400,
+        }}
+      />
+      <Boxy mobile={media.sm}>
+        <H3>{title}</H3>
+        {descriptions.map((desc, index) => (
+          <H4 key={index}>{desc}</H4>
+        ))}
+      </Boxy>
+    </YStack>
+  </Anchor>
+)
+
+const PROJECTS = [
+  {
+    href: "https://goingofftrack.blog",
+    imgSrc: img.goingofftrack.src,
+    title: "A blog website",
+    descriptions: ['ChatGPT summaries', 'Responsive layout']
+  },
+  {
+    href: "https://www.npmjs.com/package/chainedcss",
+    imgSrc: img.chainedcss.src,
+    title: "CSS-in-JS",
+    descriptions: ['Tailwind Style', 'NPM Package']
+  },
+  {
+    href: "https://elaborate-please.site/",
+    imgSrc: img.elaborate.src,
+    title: "AI Powered reading",
+    descriptions: ['Language learning']
+  },
+  {
+    href: "https://symbiote-project.web.app/",
+    imgSrc: img.symbiote.src,
+    title: "Note app",
+    descriptions: ["Google sign-in", "Firebase user data", "CRUD editable notes"]
+  }
+];
+
 export function HomePack() {
   const media = useMedia()
   return (
     <Theme name="ebb">
       <YStack w="100%" h="100%" bc="$background">
         <YStack maxWidth={1200} w="100%" mx="auto" bc="" borderColor="$blue1Dark">
-          <YStack f={1} mt="$10" jc="center" >
+          <YStack f={1} mt="$10" jc="center">
             <YStack minHeight={500}>
               <YStack flexWrap="wrap" w="100%" ai="flex-start" px={50}>
-                <Paragraph fontWeight="bold" mb={50}>Built by Adomas Gaudiesius</Paragraph>
+                <Paragraph fontWeight="bold" mb={50}>
+                  Built by Adomas Gaudiesius
+                </Paragraph>
                 <H1 mb={100} fontSize={media.sm ? 30 : undefined} letterSpacing={'normal'}>
                   Full-Stack Developer <br /> Content Creator
                 </H1>
@@ -179,75 +234,19 @@ export function HomePack() {
           <YStack maxWidth={1200} w="100%" mx="auto" py={100} p="$4" mb={100}></YStack>
           <YStack maxWidth={800} w="100%" mx="auto" p="$4">
             <H1 mb={150}>Some of my projects</H1>
-                    <Anchor href="https://goingofftrack.blog" target="_blank">
-                      {/* <H2>My blog website</H2> */}
-                      <YStack position="relative" mb={300}>
-                        <Image
-                          source={{
-                            uri: img.goingofftrack.src,
-                            width: '100%',
-                            height: media.sm ? 200 : 400,
-                          }}
-                        />
-                        <Boxy mobile={media.sm}>
-                          {/* <H3>ChainedCSS</H3> */}
-                          <H3>A blog website</H3>
-                          <P1></P1>
-                        </Boxy>
-                      </YStack>
-                    </Anchor>
-            <Anchor href="https://www.npmjs.com/package/chainedcss" target="_blank">
-              {/* <H2>Jss tailwind mix</H2> */}
-              <YStack position="relative" mb={300}>
-                <Image
-                  source={{
-                    uri: img.chainedcss.src,
-                    width: '100%',
-                    height: media.sm ? 100 : 200,
-                  }}
-                />
-                <Boxy mobile={media.sm}>
-                  <H3>ChainedCSS</H3>
-                </Boxy>
-              </YStack>
-            </Anchor>
-            <Anchor href="https://elaborate-please.site/" target="_blank">
-              {/* <H2>ChatGPT powered book</H2> */}
-              <YStack position="relative" mb={300}>
-                <Image
-                  source={{
-                    uri: img.elaborate.src,
-                    width: '100%',
-                    height: media.sm ? 200 : 400,
-                  }}
-                />
-                <Boxy mobile={media.sm}>
-                  <H3>AI</H3>
-                  <H3>A blog website</H3>
-                  <P1></P1>
-                </Boxy>
-              </YStack>
-            </Anchor>
-            <Anchor href="https://symbiote-project.web.app/" target="_blank" mb={200}>
-              {/* <H2>Note taking app with login</H2> */}
-              <YStack position="relative" mb={300}>
-                <Image
-                  source={{
-                    uri: img.symbiote.src,
-                    width: '100%',
-                    height: media.sm ? 125 : 250,
-                  }}
-                />
-                <Boxy mobile={media.sm}>
-                  <H3>Google signin </H3>
-                  <H3>Different User info</H3>
-                  <H3>Note taking</H3>
-                </Boxy>
-              </YStack>
-            </Anchor>
+            {PROJECTS.map((project, index) => (
+              <Project
+                key={index}
+                href={project.href}
+                imgSrc={project.imgSrc}
+                title={project.title}
+                descriptions={project.descriptions}
+                media={media}
+              />
+            ))}
             <YStack ai="center">
-              <Theme name="dark">
-                <SheetDemo />
+              <Theme name="light">
+                <CVSheet />
               </Theme>
             </YStack>
           </YStack>
@@ -274,8 +273,10 @@ export function HomePack() {
             <Anchor href="https://github.com/adomasgaudi" target="_blank">
               <H1>Github</H1>
             </Anchor>
-            <H2 fontSize={media.sm ? "$9" : "$10"}>+370 61609911</H2>
-            <H2 fontSize={media.sm ? "$8" : "$10"} letterSpacing={-1}>adomas.gaudi.23@gmail.com</H2>
+            <H2 fontSize={media.sm ? '$9' : '$10'}>+370 61609911</H2>
+            <H2 fontSize={media.sm ? '$8' : '$10'} letterSpacing={-1}>
+              adomas.gaudi.23@gmail.com
+            </H2>
           </YStack>
         </YStack>
       </YStack>
