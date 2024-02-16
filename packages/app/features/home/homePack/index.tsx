a4bac0b79d13b99d600f959b6051d794a13783bd
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2024-02-16 13:20:57 +0200

    fx: remove old mobile

--- packages/app/features/home/homePack/index.tsx
+++ packages/app/features/home/homePack/index.tsx
@@ -55,63 +55,6 @@ const Desktop = ({ mobile, xs }) => {
     </Theme>
   )
 }
-const MobileOld = () => {
-  const { data, fetchData } = useOpenAI()
-  const [inputValue, setInputValue] = useState('')
-  const linkProps = (href) => useLink({ href })
-
-  return (
-    <Layout1>
-      <YStack bg="$background">
-        <XStack w="full" h={60} mb={20} bg="#019564" jc="center" ai="center">
-          <P1 color="white" fontWeight="bold">
-            Mobile View
-          </P1>
-        </XStack>
-
-        {ARTICLES.map((article, index) => (
-          <YStack key={index} mt={4} mb={10}>
-            <Image
-              source={{
-                uri: article.image,
-                width: '100%',
-                height: 200,
-              }}
-              mb="$2"
-              cursor="pointer"
-              {...linkProps(article.link)}
-            />
-            <ArticleBoxSM>
-              <YStack>
-                <XStack gap={20}>
-                  <H5>Research: 5A</H5>
-                  <H5>Writing: 5A</H5>
-                </XStack>
-                <H2 mb="$2" cursor="pointer" {...linkProps(article.link)}>
-                  {article.title[0]}
-                </H2>
-                <P1>{data ? data : article.text[0]}</P1>
-                <XStack jc="center" ai="center" gap={3} mt={3}>
-                  <Input
-                    size="$2"
-                    borderWidth={1}
-                    borderColor="#cacaca"
-                    outlineColor="none"
-                    outlineWidth={0}
-                    value={inputValue}
-                    width="80%"
-                    onChangeText={(text) => setInputValue(text)}
-                  />
-                  <Button onPress={() => fetchData(inputValue)}>Go!</Button>
-                </XStack>
-              </YStack>
-            </ArticleBoxSM>
-          </YStack>
-        ))}
-      </YStack>
-    </Layout1>
-  )
-}
 
 export function HomePack() {
   const media = useMedia()
